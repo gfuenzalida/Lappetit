5430807378c183371f74c4202a0b509bfe451576
gfuenzalida <gary.fuenzalida@usach.cl>
2012-11-18 02:28:03 -0300

    Cambios menores

--- L-Appetit/Controllers/AccountController.cs
+++ L-Appetit/Controllers/AccountController.cs
@@ -94,9 +94,12 @@ namespace L_Appetit.Controllers
                 }
                 LinqDBDataContext db = new LinqDBDataContext();
                 CLIENTE c1 = new CLIENTE { RUT_CLI = model.RUT, NOMBRE_CLI = nombre, SEXO_CLIENTE = sexo, CORREO_CLI = model.Email, TELEFONO_CLI = Convert.ToInt32(model.Telefono), TICKETS_RECIBIDOS = 0 };
-                CUENTA_USUARIO u1 = new CUENTA_USUARIO {RUT_CLI = model.RUT, USERNAME = nombre, PASSWORD = model.Password };
+                //CUENTA_USUARIO u1 = new CUENTA_USUARIO {RUT_CLI = model.RUT, USERNAME = nombre, PASSWORD = model.Password };
+
+                //CLIENTE c1 = new CLIENTE { RUT_CLI = "17490314-k", NOMBRE_CLI = "sdsd", SEXO_CLIENTE = true, CORREO_CLI = "edede", TELEFONO_CLI = 3234343, TICKETS_RECIBIDOS = 0 };
+                
                 db.CLIENTE.InsertOnSubmit(c1);
-                db.CUENTA_USUARIO.InsertOnSubmit(u1);
+                //db.CUENTA_USUARIO.InsertOnSubmit(u1);
                 db.SubmitChanges();
 
                 if (createStatus == MembershipCreateStatus.Success)
