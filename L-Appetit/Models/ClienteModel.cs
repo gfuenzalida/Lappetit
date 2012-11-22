0ba8224bc481a75b16fe54766a1e592b7a899762
gfuenzalida <gary.fuenzalida@usach.cl>
2012-11-22 20:57:48 -0300

    Consultar Menu query linq

--- L-Appetit/Models/ClienteModel.cs
+++ L-Appetit/Models/ClienteModel.cs
@@ -13,7 +13,7 @@ namespace L_Appetit.Models.Cliente
         public List<string> ListaBebestible { get; set; }
         public List<string> ListaPostre { get; set; }
 
-        public void GetItems(DateTime date)
+        public void GetItems(DateTime fecha)
         {
             ListaEntrada = new List<string>();
             ListaFondo = new List<string>();
@@ -23,26 +23,42 @@ namespace L_Appetit.Models.Cliente
             //DateTime fecha = Convert.ToDateTime(date);
             bool horario = true;
             LinqDBDataContext db = new LinqDBDataContext();
-            var menu = from item in db.ITEM
-                       from menu_fecha in db.MENU_FECHA
-                       from tipo_item in db.TIPO_ITEM
-                       where
-                         menu_fecha.FECHA == date &&
-                         menu_fecha.HORARIO == horario &&
-                         item.CODIGO_ITEM == menu_fecha.CODIGO_ITEM &&
-                         tipo_item.CODIGO_TIPO_ITEM == item.CODIGO_TIPO_ITEM
-                       select new
-                       {
-                           tipo_item.NOMBRE_TIPO_ITEM,
-                           item.NOMBRE_ITEM
-                       };
+
+            var menu = (from item in db.ITEM
+                        from menu_fecha in db.MENU_FECHA
+                        from tipo_item in db.TIPO_ITEM
+                        where
+                            menu_fecha.FECHA == fecha &&
+                            menu_fecha.HORARIO == horario &&
+                            item.CODIGO_ITEM == menu_fecha.CODIGO_ITEM &&
+                            tipo_item.CODIGO_TIPO_ITEM == item.CODIGO_TIPO_ITEM
+                        select new
+                        {
+                            tipo_item.NOMBRE_TIPO_ITEM,
+                            item.NOMBRE_ITEM
+                        }
+                        ).Union((
+                        from item in db.ITEM
+                        from tipo_item in db.TIPO_ITEM
+                        where
+                            tipo_item.CODIGO_TIPO_ITEM == 4 &&
+                            item.CODIGO_TIPO_ITEM == 4
+
+                        select new
+                        {
+                            tipo_item.NOMBRE_TIPO_ITEM,
+                            item.NOMBRE_ITEM
+                        }
+                        )
+                        );
+
             foreach (var item in menu)
             {
                 if (item.NOMBRE_TIPO_ITEM.Equals("Entrada"))
                 {
                     ListaEntrada.Add(item.NOMBRE_ITEM);
                 }
-                else if (item.NOMBRE_TIPO_ITEM.Equals("Plato de fondo"))
+                else if (item.NOMBRE_TIPO_ITEM.Equals("Plato de Fondo"))
                 {
                     ListaFondo.Add(item.NOMBRE_ITEM);
                 }
@@ -55,6 +71,11 @@ namespace L_Appetit.Models.Cliente
                     ListaBebestible.Add(item.NOMBRE_ITEM);
                 }
             }
+
+            //ListaEntrada.Sort();
+            //ListaFondo.Sort();
+            //ListaPostre.Sort();
+            //ListaBebestible.Sort();
         }
     }
 }
